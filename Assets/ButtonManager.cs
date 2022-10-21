d022c3538bff3055d774db3c5c41d300ca028741
Justin Tran <jhtran023@gmail.com>
2022-10-21 16:23:11 +1100

    Setting up UI
    Remove some assets from assessment 3 and Imported visual UI. No animation included.

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void Level1Load()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void Level2Load()
+    {
+        Debug.Log("NOT FOUND");
+    }
+}
